23238c06c22235e9327444964cf1c9f5050ce690
Arch_Alejandro <carrilloalejandro0215@gmail.com>
2026-03-20 14:30:09 -0500

    Login is being worked on, using usenavigate to redirect to register route, now gotta make a req to the server to login client and save token to LocalStorage and redirect to home

--- client/README.md
+++ client/README.md
@@ -1,3 +1,4 @@
 # Install react Router dom 
 # Tree is src and then pages for the auth pages and app for the home page
 # Once client is connected we read the token saved when client logged on. Then i create the socket 
+# I use navigate cause i cannot render a route and a router in the return of a function, so import Usenavigate and instance it, then use it passing the router where i want to navigate to
--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
+import { Navigate, Routes, Route, useNavigate } from "react-router-dom"
+import Register from './Register'
 
 
 
 function Login(){
 
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate() // Using this i can navigate to another route, a function cant render JSX
+
+
+
+
+    function handleUsername(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePassword(event){
+        setPassword(event.target.value)
+    }
+
+
+    function handleSubmit(){
+
+    }
+
+    function handleRegister(){
+        navigate('/register')
+    }
 
 
 
@@ -11,6 +37,10 @@ function Login(){
     return(
         <div>
             <h1>Hellooo</h1>
+            <input value={username} onChange={handleUsername} type="text" placeholder="Enter Username" />
+            <input value={password} onChange={handlePassword} type="password" placeholder="Enter Password" />
+            <button onClick={handleSubmit} >Submit</button>
+            <button onClick={handleRegister} >Register</button>
         </div>
     )
 }
