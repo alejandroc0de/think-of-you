bd209942b480ed1e18ca14097591b3a25671a985
Arch_Alejandro <carrilloalejandro0215@gmail.com>
2026-03-21 23:58:25 -0500

    Use effect is setup for sending the token to the backend once client hits home. Also listener for incomming messages to the client

--- client/README.md
+++ client/README.md
@@ -3,4 +3,7 @@
 # Once client is connected we read the token saved when client logged on. Then i create the socket 
 # I use navigate cause i cannot render a route and a router in the return of a function, so import Usenavigate and instance it, then use it passing the router where i want to navigate to
 
-# Cors is blocking fetch request from the frontend, so i added cors to the server, and specify the route that will be allowed, it can be found un ENV so i can update it later once deployed
+# Cors is blocking fetch request from the frontend, so i added cors to the server, and specify the route that will be allowed, it can be found un ENV so i can update it later once deployed, THere are 2 cors, one for socket and another for express routes  
+# From react home i call io to pass the token so i need cors for socket io as well 
+
+# Everything outside the component is called when imported so the sending of the token has to happen once the component is mounted, hence inside the component using a useEffect 
--- client/src/pages/app/Home.jsx
+++ client/src/pages/app/Home.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
 import {io} from 'socket.io-client'
 
-const socket = io("http://localhost:3000", {
-    auth : {"token": "token"}
-})
 
 function Home(){
 
+    // DECLARATIONS ---------------------------------------------------
+    const socketInfo = useRef(null)
+    const [lastMessage,setLastMessage] = useState({sender:null,time:null,message:null})
 
+
+
+
+    /* When a client sucessfully connects i have to send the token to the backend, it decode and save the username and 
+    Id to the server memory, so real time messages can work. */
+    useEffect(() => {
+        const socket = io("http://localhost:3000", {
+            auth : {"token": localStorage.getItem("token")}  
+        })
+        socketInfo.current = socket
+
+        // Listener for socket incoming----
+        socket.on("send", (data) => {
+            setLastMessage({sender:data.sender, time:data.time, message:data.message})
+        })
+
+        return()=> {
+            // Cleanup: When client disconnects we need to disconnect socket 
+            socket.disconnect()
+        };
+    },[]); // Empty dependecy array, so when mounted 
+ 
+
+
+
+    // Here we need to implement socket to listen to incomming messages 
+
+    
 
     return (
 
